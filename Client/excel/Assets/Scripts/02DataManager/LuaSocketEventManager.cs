986bc45cfcaa235846659aeefbfa2e1eb16b1505
shenshaojun <370119386@qq.com>
2018-04-14 23:10:49 +0800

    //bug 修复

--- Client/excel/Assets/Scripts/02DataManager/LuaSocketEventManager.cs
+++ Client/excel/Assets/Scripts/02DataManager/LuaSocketEventManager.cs
@@ -16,8 +16,22 @@ namespace GameClient
         {
             if(null != handler)
             {
-                mEventDispatcher[eventId] = (LuaSocketEvent)Delegate.Remove((LuaSocketEvent)mEventDispatcher[eventId], handler);
-                mEventDispatcher[eventId] = (LuaSocketEvent)Delegate.Combine((LuaSocketEvent)mEventDispatcher[eventId], handler);
+                if(mEventDispatcher.ContainsKey(eventId))
+                {
+                    if(null != mEventDispatcher[eventId])
+                    {
+                        mEventDispatcher[eventId] = (LuaSocketEvent)Delegate.Remove((LuaSocketEvent)mEventDispatcher[eventId], handler);
+                        mEventDispatcher[eventId] = (LuaSocketEvent)Delegate.Combine((LuaSocketEvent)mEventDispatcher[eventId], handler);
+                    }
+                    else
+                    {
+                        mEventDispatcher[eventId] = handler;
+                    }
+                }
+                else
+                {
+                    mEventDispatcher[eventId] = handler;
+                }
                 LogManager.Instance().LogFormat("<color=#00ff00>reg net socket event {0} succeed !</color>", eventId);
             }
         }
